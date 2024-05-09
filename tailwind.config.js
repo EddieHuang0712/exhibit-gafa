51ada9e04b5417358e7db3c411b0058f95dbc521
EddieHuang0712 <1826942933@qq.com>
2024-05-10 00:27:49 +0800

    feat: build

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        fadeIn: {
+            "0%": { opacity: 0 },
+            "100%": { opacity: 1 },
+        },
+        slideRight: {
+            "0%": { transform: "translateX(-100%)" },
+            "100%": { transform: "translateX(0)" },
+        },
+        slideLeft: {
+            "0%": { transform: "translateX(100%)" },
+            "100%": { transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 1.5s ease-in-out",
+        slideRight: "slideRight 0.7s ease-in-out",
+        slideLeft: "slideLeft 0.7s ease-in-out",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
